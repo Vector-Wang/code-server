83df91048009b28ca829c70edc0e5bfe1ca2fa9a
Vector Wang <wjx0709@outlook.com>
2020-08-05 19:02:36 +0800

    add ubuntu16-cuda102-cudnn7

---
+++ entrypoint.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# start vs code server
+code-server --host 0.0.0.0 --port 80 --auth, password --user-data-dir /home/coder/project/.code-user-data --disable-telemetry &
+
+# start sshd
+/usr/sbin/sshd -D &
+
+# just keep this script running
+while [[ true ]]; do
+	sleep 1
+done
