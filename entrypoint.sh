d5bc9bb6c1fd122e14192fd3027615abd3c54442
Vector <wjx0709@outlook.com>
2020-08-07 20:42:58 +0800

    bug: remove dumb-init

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # start vs code server
-code-server --host 0.0.0.0 --port 80 --auth, password --user-data-dir /home/coder/project/.code-user-data --disable-telemetry &
+code-server --host 0.0.0.0 --port 80 --auth password --user-data-dir /home/coder/project/.code-user-data --disable-telemetry &
 
 # start sshd
 /usr/sbin/sshd -D &
